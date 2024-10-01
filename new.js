419bf2a4a7aa1145aafd071b5eec7ff20ecb162f
Chiranjeevi sai charan <saicharan922014@gmail.com>
2024-10-01 19:53:26 +0530

    modified

---
+++ new.js
@@ -0,0 +1,114 @@
+// Task 1
+// -------------------------
+var day="friday"
+switch (day) {
+    case "monday":
+        console.log("the day is monday");
+
+        break;
+    case "Tuesday":
+        console.log("the day is Tuesday");
+
+        break;
+    case "wedensday":
+        console.log("the day is wednesday");
+
+        break;
+    case "Thursday":
+        console.log("the day is Thursday");
+
+        break;
+    case "friday":
+        console.log("the day is friday");
+
+        break;
+    case "saturday":
+        console.log("the day is saturday");
+
+        break;
+    case "sunday":
+        console.log("the day is sunday");
+
+        break;
+
+    default:
+        console.log("not a valid value");
+
+        break;
+}
+// Task 2
+// -------------------------
+var month="sept"
+switch (month) {
+    case "Jan":
+        console.log("the month is jan");
+
+        break;
+    case "Feb":
+        console.log("the month is feb");
+
+        break;
+    case "march":
+        console.log("the month is march");
+
+        break;
+    case "Apirl":
+        console.log("the month is apirl");
+
+        break;
+    case "May":
+        console.log("the month is may");
+
+        break;
+    case "june":
+        console.log("the month is june");
+
+        break;
+    case "july":
+        console.log("the month is july");
+
+        break;
+    case "August":
+        console.log("the month is aug");
+
+        break;
+    case "sept":
+        console.log("the  month is sept");
+
+        break;
+    case "oct":
+        console.log("the month is oct");
+
+        break;
+    case "nov":
+        console.log("the month is nov");
+
+        break;
+    case "Dec":
+        console.log("the month is dec");
+
+        break;
+
+    default:
+        console.log("not a valid value");
+
+        break;
+}
+// Task 3
+// -------------------------
+var a=1;
+while (a<=10) {
+if (a%2===0){
+    console.log( a+" "+"is even no");
+
+}
+a++;
+}
+// }
+// Task 4
+// -------------------------
+var a=10;
+while (a>=1) {
+    console.log(a);
+a--;
+}
